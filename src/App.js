9bc82466864ee4986f0f0094ee8e5a1857cc1e39
amdad77682 <amad77682@gmail.com>
2019-10-01 16:10:53 +0600

    taiwind added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, useState  } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 export const App =()=> {
-
+const [count, setCount] = useState(0);
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className="bg-purple-darker m-6 p-6 rounded shadow-lg">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
